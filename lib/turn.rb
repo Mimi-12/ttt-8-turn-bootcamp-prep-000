af7a979f036cf46cafd00c98f431513481200cfe
selam lemma <oneiconforlife26@gmail.com>
2020-09-10 00:43:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,9 +1,9 @@
 def display_board(board)
-  puts "#{boared[0]} | #{boared[1]} | #{boared[2]} "
+  puts " #{boared[0]} | #{boared[1]} | #{boared[2]} "
   puts "-----------"
-  puts "#{boared[3]} | #{boared[4]} | #{boared[5]} "
+  puts " #{boared[3]} | #{boared[4]} | #{boared[5]} "
   puts "-----------"
-  puts "#{boared[6]} | #{boared[7]} | #{boared[8]} "
+  puts " #{boared[6]} | #{boared[7]} | #{boared[8]} "
 end
 def input_to_index(user_input)
    user_input.to_i - 1
