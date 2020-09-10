b443d30a713b886a18390d492e839c6210b4ea57
selam lemma <oneiconforlife26@gmail.com>
2020-09-10 01:39:55 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,6 +25,7 @@ def turn(board)
    index = input_to_index(input)
     if valid_move?(board, index)
       move(board, index)
+      display_board(board)
     else
       puts "opps! Thats not a valid move. "
       turn(board)
