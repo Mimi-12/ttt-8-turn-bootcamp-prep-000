8f6a9ce0cf7b45d33aaff822b802f9aa15cff557
selam lemma <oneiconforlife26@gmail.com>
2020-09-10 00:35:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def display_board(board)
   puts "#{boared[0]} | #{boared[1]} | #{boared[2]} "
-  puts ("-----------")
+  puts "-----------"
   puts "#{boared[3]} | #{boared[4]} | #{boared[5]} "
-  puts ("-----------")
+  puts "-----------"
   puts "#{boared[6]} | #{boared[7]} | #{boared[8]} "
 end
